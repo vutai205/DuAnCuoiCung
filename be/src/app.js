99443e799ec78e380437590ef2fa8d2658101fa2
thinhDo <thinhdo@media-one.vn>
2026-06-05 08:45:59 +0700

    Khởi tạo cấu trúc Base dự án (Frontend & Backend)

---
+++ be/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to DuAnCuoiCung Backend API' });
+});
+
+module.exports = app;
