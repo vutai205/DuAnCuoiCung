8bcc81da59e5083a4a431c6ae663a3e33684cfaa
thinhDo <thinhdo@media-one.vn>
2026-06-05 14:38:13 +0700

    Cập nhật API CRUD danh sách phim

--- be/src/controllers/movieController.js
+++ be/src/controllers/movieController.js
@@ -25,3 +25,53 @@ exports.getMovieById = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+// @desc    Create a movie
+// @route   POST /api/movies
+// @access  Private/Admin
+exports.createMovie = async (req, res) => {
+    try {
+        const movie = new Movie(req.body);
+        const createdMovie = await movie.save();
+        res.status(201).json(createdMovie);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// @desc    Update a movie
+// @route   PUT /api/movies/:id
+// @access  Private/Admin
+exports.updateMovie = async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+
+        if (movie) {
+            Object.assign(movie, req.body);
+            const updatedMovie = await movie.save();
+            res.json(updatedMovie);
+        } else {
+            res.status(404).json({ message: 'Movie not found' });
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// @desc    Delete a movie
+// @route   DELETE /api/movies/:id
+// @access  Private/Admin
+exports.deleteMovie = async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+
+        if (movie) {
+            await movie.deleteOne();
+            res.json({ message: 'Movie removed' });
+        } else {
+            res.status(404).json({ message: 'Movie not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- be/src/routes/movies.js
+++ be/src/routes/movies.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getMovies, getMovieById } = require('../controllers/movieController');
+const { getMovies, getMovieById, createMovie, updateMovie, deleteMovie } = require('../controllers/movieController');
+const { protect, admin } = require('../middlewares/authMiddleware');
 
 router.get('/', getMovies);
 router.get('/:id', getMovieById);
+
+// Admin routes
+router.post('/', protect, admin, createMovie);
+router.put('/:id', protect, admin, updateMovie);
+router.delete('/:id', protect, admin, deleteMovie);
 
 module.exports = router;
