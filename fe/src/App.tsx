b930183bf4ba593571493f23bce094f6e8189825
vutai205 <vuanhtai2k5@gmail.com>
2026-06-24 01:35:29 +0700

    update lai

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminLayout from "./app/pages/admin/AdminLayout";
+import Dashboard from "./app/pages/admin/Dashboard";
 
 import Header from "./app/components/Header/Header";
 import Footer from "./app/components/Footer/Footer";
@@ -14,6 +16,9 @@ function App() {
       <Header />
 
       <Routes>
+        <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<Dashboard />} />
+      </Route>
         <Route path="/" element={<Profile />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/member-card" element={<MemberCard />} />
