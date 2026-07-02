30326d388bfe39a96231d864b19a9699eacf6c0b
chinhnxph45367 <166044002+chinhnxph45367@users.noreply.github.com>
2026-07-03 00:44:44 +0700

    Create Movie.ts

---
+++ fe/src/types/Movie.ts
@@ -0,0 +1,12 @@
+export interface Movie {
+  _id?: string;
+  title: string;
+  description: string;
+  duration: number;
+  genre: string;
+  language: string;
+  releaseDate: string;
+  poster: string;
+  trailer: string;
+  status: boolean;
+}
