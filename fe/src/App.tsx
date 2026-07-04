dcb578b8263e5581bfe6b3730a45efd655063d32
Pham Van Tu <phamvantu@example.com>
2026-07-04 13:17:03 +0700

    fix router + update App.tsx and main.tsx

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,18 +1,32 @@
 ﻿import { Routes, Route, Link } from "react-router-dom";
 import MoviesPage from "./pages/MoviesPage";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
+import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
+
 function Home() {
   return (
     <div>
       <h1>CineTicket</h1>
-      <Link to="/movies">Xem phim</Link>
+
+      <nav style={{ display: "flex", gap: "10px" }}>
+        <Link to="/movies">Xem phim</Link>
+        <Link to="/login">Đăng nhập</Link>
+        <Link to="/register">Đăng ký</Link>
+        <Link to="/forgot-password">Quên mật khẩu</Link>
+      </nav>
     </div>
   );
 }
+
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/movies" element={<MoviesPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
     </Routes>
   );
 }
